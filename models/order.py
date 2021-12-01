d5b023f080c7a82d6278d3acf7de9a7611aa3607
garyb6 <gary.bennett6@outlook.com>
2021-12-01 13:43:49 +0000

    created 3 orders

--- models/order.py
+++ models/order.py
@@ -1,3 +1,5 @@
-from models.order import *
+from models.book import *
 
-book1 = Order()
+order1 = Order("Mitchell Pritchard", "01/11/2021", 1, "The_Last_Ship", "war")
+order2 = Order("Clare Dunphy", "02/11/2021", 2, "Closet_Fan", "drama")
+order3 = Order("Alex Dunphy", "03/11/2021", 3, "Vespasian", "historical_fiction" )
