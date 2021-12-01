ce408203da010fc09530151839ab9588f644843d
garyb6 <gary.bennett6@outlook.com>
2021-12-01 13:51:41 +0000

    filled in initial information in python modules

---
+++ controllers/controller.py
@@ -0,0 +1,8 @@
+import re
+from app import app
+from flask import render_template
+from models.order import *
+
+@app.route('/')
+def index():
+    return render_template('index.html', title="Home", orders=orders)
